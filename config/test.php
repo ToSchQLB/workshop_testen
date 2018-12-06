f390f8fedb8869d2e5f3b8bd1fc4539f5843ee43
Toni Schreiber <toni.schreiber@jki.bund.de>
2018-12-06 08:23:34 +0100

    test_config

--- config/test.php
+++ config/test.php
@@ -12,7 +12,7 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'language' => 'en-US',
+    'language' => 'de-DE',
     'components' => [
         'db' => $db,
         'mailer' => [
@@ -37,6 +37,37 @@ return [
             ],
             */
         ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => \yii\i18n\PhpMessageSource::className(),
+                    'basePath' => '@app/messages',
+//                    'fileMap' => [
+//                        'projekt' => 'projekt.php'
+//                    ]
+                ],
+            ]
+        ],
     ],
     'params' => $params,
+    'modules' => [
+        'user' => [
+            'class' => Da\User\Module::className(),
+            'routes' => [
+                '<id:\d+>' => 'profile/show',
+                '<action:(login|logout)>' => 'security/<action>',
+                '<action:(register|resend)>' => 'registration/<action>',
+                'confirm/<id:\d+>/<code:[A-Za-z0-9_-]+>' => 'registration/confirm',
+                'forgot' => 'recovery/request',
+                'recover/<id:\d+>/<code:[A-Za-z0-9_-]+>' => 'recovery/reset',
+                'settings/<action:\w+>' => 'settings/<action>',
+            ],
+            'generatePasswords' => false,
+//            'administrators' => ['toni.schreiber'],
+            'administratorPermissionName' => 'Admin',
+            // ...other configs from here: [Configuration Options](installation/configuration-options.md), e.g.
+            // 'generatePasswords' => true,
+            // 'switchIdentitySessionKey' => 'myown_usuario_admin_user_key',
+        ],
+    ]
 ];
